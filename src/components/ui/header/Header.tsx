4811c69970f6678aa3ef28cf83466db805c33d04
Brandon Lee Schultz <schultzblee@gmail.com>
2025-02-26 13:47:44 +0200

    fix(metadata): Update favicon and logo svg

--- src/components/ui/header/Header.tsx
+++ src/components/ui/header/Header.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import Image from 'next/image'
 
 type HeaderProps = {
     title: string
@@ -7,10 +8,21 @@ type HeaderProps = {
 
 export default function Header({title, titleSubpart}: HeaderProps) {
     return (
-        <div className="mt-5">
-            <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span
-                className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">{title} </span> {titleSubpart}
+        <div className="mt-5 flex flex-col items-center justify-center">
+            <Image
+                className="mb-4"
+                src="/c-i-t-b.svg"
+                alt="Logo"
+                width={100}
+                height={100}
+            />
+            <h1 className="text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
+        <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+            {title}
+        </span>
+                {titleSubpart}
             </h1>
         </div>
+
     )
 }

28933c4d887d5fe2a3a9b9ab06ecbe3544e2628f
Brandon Lee Schultz <schultzblee@gmail.com>
2025-02-26 12:59:58 +0200

    chore(cleanup): Add gradient to footer text

--- src/components/ui/footer/Footer.tsx
+++ src/components/ui/footer/Footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Footer() {
     return (
         <footer className="flex gap-6 flex-wrap items-center justify-center mt-10">
-            <small>Created by <Link href="/credits">Brandon-Lee Schultz</Link></small>
+            <small>Created by <Link href="/credits" className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Brandon-Lee Schultz</Link></small>
         </footer>
     )
 }
